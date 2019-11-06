0468eefda373b63bc3c4f534db4450789abd2321
Astrid Sutton <astrid.sutton@wgu.edu>
2019-11-06 15:14:46 -0700

    completed: callback pattern,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_01/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_01/start/index.js
@@ -1,7 +1,26 @@
-function hideString(str) {
-    return str.replace(/[a-zA-Z]/g, 'X');
+// function hideString(str, done) {
+//     process.nextTick(() => {
+//       done(str.replace(/[a-zA-Z]/g, 'X'));
+//     })
+// }
+
+// hideString("Hello World", (hidden) => {
+//   console.log( hidden );
+// });
+
+// console.log( 'end' );
+
+function delay(seconds, callback) {
+  setTimeout(callback, seconds * 1000);
 }
 
-var hidden = hideString("Hello World");
-
-console.log( hidden );
+console.log('starting delays');
+delay(2, () => {
+  console.log('2 sec');
+  delay(1, () => {
+    console.log('3 sec');
+    delay(1, () => {
+      console.log('4 sec');
+    })
+  })
+})
