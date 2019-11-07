021b5e131e8d454095382516af577929c00f0442
Astrid Sutton <astrid.sutton@wgu.edu>
2019-11-07 10:30:36 -0700

    completed: resolving promises,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_02/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_02/start/index.js
@@ -1,9 +1,20 @@
-function delay(seconds, callback) {
-    setTimeout(callback, seconds*1000);
-}
-
-delay(1, () => {
-    console.log('one second');
+var delay = (seconds) => new Promise((resolves, rejects) => {
+  setTimeout(() => {
+    resolves('the long delay has ended');
+  }, seconds * 1000);
 })
+
+// function delay(seconds, callback) {
+//     setTimeout(callback, seconds * 1000);
+// }
+
+// delay(1, () => {˝
+//     console.log('one second');
+// })
+
+delay(1)
+  .then(console.log)
+  .then(() => 42)
+  .then((response) => console.log(`chained: ${response}`))
 
 console.log('end first tick');
