49f5858dc622fe9d3ac9add6a59fb148799b59ad
Astrid Sutton <astrid.sutton@wgu.edu>
2019-12-17 13:26:13 -0700

    completed: the promisify function,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_04/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_04/start/index.js
@@ -1,18 +1,33 @@
-var delay = (seconds, callback) => {
-    if (seconds > 3) {
-        callback(new Error(`${seconds} seconds it too long!`));
-    } else {
-        setTimeout(() =>
-            callback(null, `the ${seconds} second delay is over.`),
-            seconds
-        );
-    }
-}
+var fs = require('fs');
+var { promisify } = require('util');
+var writeFile = promisify(fs.writeFile);
 
-delay(2, (error, message) => {
-    if (error) {
-        console.log(error.message);
-    } else {
-        console.log(message);
-    }
-});
+// var delay = (seconds, callback) => {
+//     if (seconds > 3) {
+//         callback(new Error(`${seconds} seconds is too long!`));
+//     } else {
+//         setTimeout(() =>
+//             callback(null, `the ${seconds} second delay is over.`),
+//             seconds
+//         );
+//     }
+// }
+
+// delay(4, (error, message) => {
+//     if (error) {
+//         console.log(error.message);
+//     } else {
+//         console.log(message);
+//     }
+// });
+
+// var promiseDelay = promisify(delay);
+
+// promiseDelay(5)
+//   .then(console.log)
+//   .catch(error => console.log(`error: ${error.message}`))
+
+// write file (text file, text)
+writeFile('sample text', 'this is a sample')
+  .then(() => console.log('successful'))
+  .catch(error => console.log('error'))
