5e676728e87354a1d4251add814ec64fb3e0ccb7
Astrid Sutton <astrid.sutton@wgu.edu>
2019-12-17 14:26:13 -0700

    completed: parallel execution,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_07/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_07/start/index.js
@@ -7,3 +7,18 @@ var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
 })
+
+Promise.race([
+  // writeFile('readme.md', 'hello world'),
+  // writeFile('readme.txt', 'hello world'),
+  // writeFile('readme.json', '{ "hello": "world" }'),
+  // unlink('readme.md'),
+  // unlink('readme.txt'),
+  // unlink('readme.json'),
+  delay(5),
+  delay(2),
+  delay(3),
+  delay(5),
+])
+  .then(() => readdir(__dirname))
+  .then(console.log)
