3455266c3b1eebd676affefaf47c1fd7f9132214
Astrid Sutton <astrid.sutton@wgu.edu>
2019-12-17 14:03:58 -0700

    completed: sequential execution with async/await,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_06/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_06/start/index.js
@@ -2,29 +2,56 @@ var fs = require('fs');
 var { promisify } = require('util');
 var writeFile = promisify(fs.writeFile);
 var unlink = promisify(fs.unlink);
+var readdir = promisify(fs.readdir);
 var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
 })
 
-const doStuffSequentially = () => Promise.resolve()
-  .then(() => console.log('starting'))
-  .then(() => delay(1))
-  .then(() => 'waiting')
-  .then(console.log)
-  .then(() => delay(2))
-  .then(() => writeFile('file.txt', 'Sample File...'))
-  .then(beep)
-  .then(() => 'file.txt created')
-  .then(console.log)
-  .then(() => delay(3))
-  .then(() => unlink('file.txt'))
-  .then(beep)
-  .then(() => 'file.txt removed')
-  .then(console.log)
-  .catch(console.error);
+async function start() {
+  var files = await readdir(__dirname);
+  console.log(files);
+}
 
-doStuffSequentially()
-  .then(() => console.log('again again!!!'))
-  .then(() => doStuffSequentially())
-  .then(() => console.log('enough already...'));
+start();
+
+// const doStuffSequentially = async () => {
+//   console.log('start');
+//   await delay(1);
+//   console.log('waiting');
+//   await delay(2);
+//   try {
+//     await writeFile('file.txt', 'sample file...');
+//     beep();
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   console.log('file.txt created');
+//   await delay(3);
+//   await unlink('file.txt');
+//   beep();
+//   console.log('file.txt removed');
+
+//   return Promise.resolve();
+// }
+
+  // .then(() => console.log('starting'))
+  // .then(() => delay(1))
+  // .then(() => 'waiting')
+  // .then(console.log)
+  // .then(() => delay(2))
+  // .then(() => writeFile('file.txt', 'Sample File...'))
+  // .then(beep)
+  // .then(() => 'file.txt created')
+  // .then(console.log)
+  // .then(() => delay(3))
+  // .then(() => unlink('file.txt'))
+  // .then(beep)
+  // .then(() => 'file.txt removed')
+  // .then(console.log)
+  // .catch(console.error);
+
+// doStuffSequentially()
+  // .then(() => console.log('again again!!!'))
+  // .then(() => doStuffSequentially())
+  // .then(() => console.log('enough already...'));
