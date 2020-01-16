c436bcf34c3898d5a01a091591896f55a1427e28
Astrid Sutton <astrid.sutton@wgu.edu>
2020-01-16 10:01:18 -0700

    completed: concurrent tasks,

--- Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_08/start/index.js
+++ Ex_Files_Advanced_NodeJS/Exercise Files/Ch01/01_08/start/index.js
@@ -14,3 +14,34 @@ var tasks = [
   delay(3),
   delay(5)
 ];
+
+class PromiseQueue {
+  constructor(
+    promises = [],
+    concurrentCount = 1,
+  ) {
+    this.concurrentCount = concurrentCount;
+    this.total = promises.length;
+    this.todo = promises;
+    this.running = [];
+    this.complete = [];
+  }
+
+  get runAnother() {
+    return (this.running.lenth < this.concurrentCount) && this.todo.length;
+  }
+
+  run() {
+    while (this.runAnother) {
+      var promise = this.todo.shift;
+      promise.then(() => {
+        this.complete.push(this.running.shift);
+        this.run();
+      })
+      this.running.push(promise);
+    }
+  }
+}
+
+var delayQ = new PromiseQueue(tasks, 2);
+delayQ.run();
